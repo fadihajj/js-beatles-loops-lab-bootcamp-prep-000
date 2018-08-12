cfbf7e42103aad45733161485fc2b5353478378d
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-12 17:00:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musArr, instArr){
+  var emptyArr = [];
+  
+  for (var i=0; i<musArr.length; i++)
+  {
+    emptyArr.push(musArr[i] + " plays " + instArr[i]);
+  }
+  return emptyArr;
+}
+
+function johnLennonFacts(factsArr){
+  
+  var i=0;
+  while (i <= factsArr.length){
+    factsArr[i] += '!!!';
+    i++;
+  }
+  return factsArr;
+}
